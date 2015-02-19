e52a00da22db21cce95acd14505b4fe999170571
Jon Stevens <latchkey@gmail.com>
2015-02-19 10:41:45 -0800

    add karma

---
+++ src/tasks/karma.js
@@ -0,0 +1,22 @@
+var karma = require('karma').server;
+
+class KarmaTask {
+  setOptions(options) {
+    this.options = options;
+
+    if (!this.options.configFile) {
+      throw new Error('KarmaTask: configFile is missing from configuration!');
+    }
+
+    return this;
+  }
+
+  defineTask(gulp) {
+    let options = this.options;
+    gulp.task(options.taskName, options.taskDeps, function (done) {
+      karma.start({configFile: options.configFile}, done);
+    });
+  }
+}
+
+module.exports = new KarmaTask();
