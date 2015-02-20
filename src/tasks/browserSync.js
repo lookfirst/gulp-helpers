03c08256e7fc317e3abcedff964268e2ba38ef78
Jon Stevens <latchkey@gmail.com>
2015-02-19 16:41:18 -0800

    add browserSync

---
+++ src/tasks/browserSync.js
@@ -0,0 +1,35 @@
+var browserSync = require('browser-sync');
+var historyApiFallback = require('connect-history-api-fallback');
+
+class BrowserSyncTask {
+  setOptions(options) {
+    this.options = options;
+
+    if (!this.options.config) {
+      throw new Error('BrowserSyncTask: config is missing from configuration!');
+    }
+
+    if (this.options.historyApiFallback) {
+      if (!this.options.config.server) {
+        this.options.config.server = {};
+      }
+      if (!this.options.config.server.middleware) {
+        this.options.config.server.middleware = [];
+      }
+      this.options.config.server.middleware.push(historyApiFallback);
+    }
+
+    return this;
+  }
+
+  defineTask(gulp) {
+    let options = this.options;
+    gulp.task(options.taskName, options.taskDeps, function () {
+      new Promise((resolve) => {
+        browserSync(options.config, resolve);
+      })
+    });
+  }
+}
+
+module.exports = new BrowserSyncTask();
