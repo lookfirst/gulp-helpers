48d6b7e1afc09fc30338d9be75862d4fdd979e03
Jon Stevens <latchkey@gmail.com>
2015-03-08 18:44:30 -0700

    add sass support

---
+++ src/tasks/sass.js
@@ -0,0 +1,46 @@
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var cache = require('gulp-cached');
+var changed = require('gulp-changed');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync');
+
+class SassTask {
+	setOptions(options) {
+		this.options = options;
+
+		if (!this.options.src) {
+			throw new Error('SassTask: src is missing from configuration!');
+		}
+
+		if (!this.options.dest) {
+			throw new Error('SassTask: dest is missing from configuration!');
+		}
+
+		this.options.extension = '.scss';
+		if (this.options.config) {
+			if (this.options.config.indentedSyntax) {
+				this.options.extension = '.sass';
+			}
+		}
+
+		return this;
+	}
+
+	defineTask(gulp) {
+		let options = this.options;
+		gulp.task(options.taskName, options.taskDeps, function() {
+			return gulp.src(options.src)
+				.pipe(cache(options.taskName))
+				.pipe(plumber())
+				.pipe(changed(options.dest, {extension: options.extension}))
+				.pipe(sourcemaps.init())
+				.pipe(sass(options.config))
+				.pipe(sourcemaps.write('.'))
+				.pipe(gulp.dest(options.dest))
+				.pipe(browserSync.reload({stream: true}));
+		});
+	}
+}
+
+module.exports = new SassTask();
