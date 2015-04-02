31e1aabf7685bba271e62e2ce47795ed93cc706f
Jon Stevens <latchkey@gmail.com>
2015-04-02 00:26:57 -0700

    handle the deleted event in the watcher if the dest folder is specified.

--- src/tasks/watch.js
+++ src/tasks/watch.js
@@ -1,3 +1,5 @@
+import del from 'del';
+import path from 'path';
 import gutil from 'gulp-util';
 import _isUndefined from 'lodash/lang/isUndefined';
 
@@ -20,9 +22,16 @@ class WatchTask {
 		let options = this.options;
 		gulp.task(options.taskName, options.taskDeps, () => {
 			let watcher = gulp.watch(options.src, options.tasks);
-			watcher.on('change', (event) =>
-				gutil.log(gutil.colors.magenta(`File ${event.path} was ${event.type}, running tasks...`))
-			);
+			watcher.on('change', (event) => {
+				gutil.log(gutil.colors.magenta(`File ${event.path} was ${event.type}, running tasks...`));
+
+				// https://github.com/gulpjs/gulp/blob/master/docs/recipes/handling-the-delete-event-on-watch.md
+				if (event.type === 'deleted' && !_isUndefined(options.dest)) {
+					var filePathFromSrc = path.relative(path.resolve(options.src), event.path);
+					var destFilePath = path.resolve(options.dest, filePathFromSrc);
+					del.sync(destFilePath);
+				}
+			});
 		});
 	}
 }
